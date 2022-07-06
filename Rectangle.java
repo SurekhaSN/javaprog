cf4d1105ed12578c8bbc531714d0f1b74aac61a1
Surekha <Surekharaj43@gmail.com>
2022-07-06 13:27:53 +0530

    Adding new java program

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle{
+
+public static void main(String args[]){
+
+int width=5;
+int height=10;
+int area=width*height;
+
+System.out.println("Area of Rectangle="+area);
+
+}
+
+}
